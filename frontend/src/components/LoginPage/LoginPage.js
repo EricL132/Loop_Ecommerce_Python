46ce885dff0e0ecb048b3e7d615d4612bc31e225
Eric <ericliao132@gmail.com>
2022-03-20 00:10:39 -0400

    fixed hit enter to submit forms

--- frontend/src/components/LoginPage/LoginPage.js
+++ frontend/src/components/LoginPage/LoginPage.js
@@ -1,49 +1,82 @@
-import "./LoginPage.css"
-import {Link} from 'react-router-dom'
-import {useState} from 'react'
-import {useHistory} from 'react-router'
-import {useDispatch} from 'react-redux'
-import {LoggedIn} from '../../redux/actions/index'
+import "./LoginPage.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useHistory } from "react-router";
+import { useDispatch } from "react-redux";
+import { LoggedIn } from "../../redux/actions/index";
 export default function LoginPage() {
-    const [formInfo,setFormInfo] = useState({
-        email:"",
-        password:""
-    })
-    const [errorMessage,setErrorMessage] = useState()
-    const history = useHistory()
-    const dispatch = useDispatch()
-    function login(e){
-        e.preventDefault()
-        setErrorMessage("")
-        for(var i in formInfo){
-            if(!formInfo[i]) return setErrorMessage("Invalid Login Info")
+    const [formInfo, setFormInfo] = useState({
+        email: "",
+        password: "",
+    });
+    const [errorMessage, setErrorMessage] = useState();
+    const history = useHistory();
+    const dispatch = useDispatch();
+    function login(e) {
+        e.preventDefault();
+        setErrorMessage("");
+        for (var i in formInfo) {
+            if (!formInfo[i]) return setErrorMessage("Invalid Login Info");
         }
-        if(!formInfo["email"].includes(".") || !formInfo["email"].includes("@")) return setErrorMessage("Invalid email")
-        if(formInfo["password"].length<6) return setErrorMessage("Invalid Password")
-        fetch("/api/login",{method:"POST",headers:{"content-type":"application/json"},body:JSON.stringify(formInfo)}).then((res)=>{
-            if(res.ok) {
-                dispatch(LoggedIn())
-                history.push("/account/info")
-            }
-            return res.text()
-        }).then((data)=>{
-            setErrorMessage(data)
+        if (!formInfo["email"].includes(".") || !formInfo["email"].includes("@"))
+            return setErrorMessage("Invalid email");
+        if (formInfo["password"].length < 6) return setErrorMessage("Invalid Password");
+        fetch("/api/login", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(formInfo),
         })
-    }  
+            .then((res) => {
+                if (res.ok) {
+                    dispatch(LoggedIn());
+                    history.push("/account/info");
+                }
+                return res.text();
+            })
+            .then((data) => {
+                setErrorMessage(data);
+            });
+    }
 
-    function changeInfo(e){
-        setFormInfo((prev)=>({...prev,[e.target.id]:e.target.value}))
+    function changeInfo(e) {
+        setFormInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+    }
+    function submitHandler(e) {
+        e.preventDefault();
     }
     return (
         <div className="main">
-            <form id="login_container" onChange={changeInfo}>
-                <h1 id="login_h1">Login <Link to="/account/register"><button id="register">Register</button></Link></h1>
-                <input id="email" className="login_input" placeholder="Email"></input>
-                <input id="password" type="password" className="login_input" placeholder="Password"></input>
-                <Link to="/account/forgot"><button id="forgotpass">Forgot Password?</button></Link>
-                <button id="signin" onClick={login}>SIGN IN</button>
-                <span className="login_error">{errorMessage}</span>
-            </form>
+            <div id="login_container">
+                <div className="login-header">
+                    <h1 id="login_h1">
+                        Login{" "}
+                        <Link to="/account/register">
+                            <button id="register">Register</button>
+                        </Link>
+                    </h1>
+                </div>
+                <form onChange={changeInfo} onSubmit={submitHandler}>
+                    <input
+                        id="email"
+                        className="login_input"
+                        autocomplete="off"
+                        placeholder="Email"
+                    ></input>
+                    <input
+                        id="password"
+                        type="password"
+                        className="login_input"
+                        placeholder="Password"
+                    ></input>
+                    <button id="signin" onClick={login}>
+                        SIGN IN
+                    </button>
+                    <Link to="/account/forgot">
+                        <button id="forgotpass">Forgot Password?</button>
+                    </Link>
+                    <span className="login_error">{errorMessage}</span>
+                </form>
+            </div>
         </div>
-    )
+    );
 }
--- frontend/src/components/RegisterPage/RegisterPage.js
+++ frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,46 +1,68 @@
-import "./RegisterPage.css"
-import {Link} from 'react-router-dom'
-import { useState } from "react"
-import {useHistory} from 'react-router'
+import "./RegisterPage.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useHistory } from "react-router";
 export default function RegisterPage() {
-    const [formInfo,setFormInfo] = useState({
-        fname:"",
-        lname:"",
-        email:"",
-        password:""
-    })
-    const [errorMessage,setErrorMessage] = useState()
-    const history = useHistory()
-    function register(e){
-        e.preventDefault()
-        setErrorMessage("")
-        for(var i in formInfo){
-            if(!formInfo[i]) return setErrorMessage("Please fill out all info")
+    const [formInfo, setFormInfo] = useState({
+        fname: "",
+        lname: "",
+        email: "",
+        password: "",
+    });
+    const [errorMessage, setErrorMessage] = useState();
+    const history = useHistory();
+    function register(e) {
+        e.preventDefault();
+        setErrorMessage("");
+        for (var i in formInfo) {
+            if (!formInfo[i]) return setErrorMessage("Please fill out all info");
         }
-        if(!formInfo["email"].includes(".") || !formInfo["email"].includes("@")) return setErrorMessage("Invalid email")
-        if(formInfo["password"].length<6) return setErrorMessage("Password must be at least 6 characters")
-        fetch("/api/register",{method:"POST",headers:{"content-type":"application/json"},body:JSON.stringify(formInfo)}).then((res)=>{
-            if(res.ok) history.push("/account/info")
-            return res.text()
-        }).then((data)=>{
-            setErrorMessage(data)
+        if (!formInfo["email"].includes(".") || !formInfo["email"].includes("@"))
+            return setErrorMessage("Invalid email");
+        if (formInfo["password"].length < 6)
+            return setErrorMessage("Password must be at least 6 characters");
+        fetch("/api/register", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(formInfo),
         })
-    }  
+            .then((res) => {
+                if (res.ok) history.push("/account/info");
+                return res.text();
+            })
+            .then((data) => {
+                setErrorMessage(data);
+            });
+    }
 
-    function changeInfo(e){
-        setFormInfo((prev)=>({...prev,[e.target.id]:e.target.value}))
+    function changeInfo(e) {
+        setFormInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
     }
     return (
         <div className="main">
-            <form id="login_container" onChange={changeInfo}>
-                <h1 id="login_h1">Register <Link to="/account/login"><button id="register">Login</button></Link></h1>
-                <input id="fname" className="login_input" placeholder="First name"></input>
-                <input id="lname" className="login_input" placeholder="Last name"></input>
-                <input id="email" className="login_input" placeholder="Email"></input>
-                <input id="password" type="password" className="login_input" placeholder="Password"></input>
-                <button id="signin" onClick={register}>REGISTER</button>
-                <span className="login_error">{errorMessage}</span>
-            </form>
+            <div id="login_container">
+                <h1 id="login_h1">
+                    Register{" "}
+                    <Link to="/account/login">
+                        <button id="register">Login</button>
+                    </Link>
+                </h1>
+                <form onChange={changeInfo}>
+                    <input id="fname" className="login_input" placeholder="First name"></input>
+                    <input id="lname" className="login_input" placeholder="Last name"></input>
+                    <input id="email" className="login_input" placeholder="Email"></input>
+                    <input
+                        id="password"
+                        type="password"
+                        className="login_input"
+                        placeholder="Password"
+                    ></input>
+                    <button id="signin" onClick={register}>
+                        REGISTER
+                    </button>
+                    <span className="login_error">{errorMessage}</span>
+                </form>
+            </div>
         </div>
-    )
+    );
 }
